1afbe40ecf0f326b09caffa59c3cac360e52c98a
Saher Kawas <saher.kawas@gmail.com>
2025-03-06 17:23:06 +0200

    Buttonclicker

---
+++ Web Fundamentals/JS assignments/Buttonclicker/buttonclicker.js
@@ -0,0 +1,13 @@
+function changeText(el){
+    if(el.innerText=="Login"){
+        el.innerText="Logout"
+    }
+}
+
+function getNotification(){
+    alert("Ninja was liked")
+}
+
+function hideButton(element){
+    element.remove();
+}
