f837d0706b94d15639641812e5a9ed90ee3e998e
Saher Kawas <saher.kawas@gmail.com>
2025-03-07 16:42:53 +0200

    Videopreview

---
+++ Web Fundamentals/JS assignments/Videopreview/script.js
@@ -0,0 +1,8 @@
+function playVideo (){
+    var video= document.querySelector ("#dog-video");
+    video.play ();
+}
+function pauseVideo (){
+    var video= document.querySelector ("#dog-video");
+    video.pause();
+}
