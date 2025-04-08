1f6454dea3c450f493bde896bc9d74816917a387
Saher Kawas <saher.kawas@gmail.com>
2025-04-08 20:05:13 +0200

    HTML Table

--- python_stack/flask/flask_fundamentals/HTML Table/server.py
+++ python_stack/flask/flask_fundamentals/HTML Table/server.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template  
 app = Flask(__name__) 
 
+@app.route('/')
+def display_home():
+    return ("Welcome to the HTML table page")
+
 @app.route('/table')
 def render_table():
     users = [
