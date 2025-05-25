5ba54517fa03dd39568292f8587f98046a5d534b
SaherKawas <saher.kawas@gmail.com>
2025-05-25 15:22:54 +0300

    HelloHuman

---
+++ Java/Java Spring/HelloHuman/src/main/java/com/axsos/hellohuman/HelloHumanApplication.java
@@ -0,0 +1,32 @@
+package com.axsos.hellohuman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@SpringBootApplication
+@RestController
+public class HelloHumanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloHumanApplication.class, args);
+	}
+	
+	@RequestMapping("/")
+    public String index() {
+		return "Hello Human!";
+    }
+	
+	@RequestMapping("/greet")
+    public String name(
+    		@RequestParam(value="name", required=false) String name,
+    		@RequestParam(value="lastname", required=false) String lastname
+    		) {
+		
+		return "Hello " + name + " " + lastname;
+    }
+	
+
+}
