6d286f53b04ec9200638f9cabc8efa76b129e548
SaherKawas <saher.kawas@gmail.com>
2025-06-03 11:49:30 +0300

    BookAPI

--- Java/Java Spring/BookAPI/src/main/java/com/axsos/bookapi/controllers/BookApiController.java
+++ Java/Java Spring/BookAPI/src/main/java/com/axsos/bookapi/controllers/BookApiController.java
@@ -1,5 +1,44 @@
 package com.axsos.bookapi.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.axsos.bookapi.models.BookApi;
+import com.axsos.bookapi.services.BookApiService;
+
+import ch.qos.logback.core.joran.spi.HttpUtil.RequestMethod;
+
+@Controller
 public class BookApiController {
 
+	@Autowired
+	BookApiService bookApiService;
+	
+	@PutMapping("/{id}")
+	public BookApi update(
+	    @PathVariable("id") Long id,
+	    @RequestParam(value="title") String title,
+	    @RequestParam(value="description") String desc,
+	    @RequestParam(value="language") String lang,
+	    @RequestParam(value="pages") Integer numOfPages) {
+	    BookApi book = bookApiService.updateBook(id, title, desc, lang, numOfPages);
+	    return book;
+	}
+
+	@DeleteMapping("/{id}")
+	public void destroy(@PathVariable("id") Long id) {
+	    bookApiService.deleteBook(id);
+	}
+	
+	
 }
--- Java/Java Spring/BookAPI/src/main/java/com/axsos/bookapi/services/BookApiService.java
+++ Java/Java Spring/BookAPI/src/main/java/com/axsos/bookapi/services/BookApiService.java
@@ -3,6 +3,7 @@ package com.axsos.bookapi.services;
 
 import com.axsos.bookapi.models.BookApi;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -21,5 +22,23 @@ public class BookApiService {
 	public BookApi createBook(BookApi books){
 		return bookApiRepository.save(books);
 	}
+	
+	public BookApi updateBook(Long id, String title, String desc, String lang, Integer numOfPages) {
+        Optional<BookApi> optionalBook = bookApiRepository.findById(id);
+        if (optionalBook.isPresent()) {
+            BookApi book = optionalBook.get();
+            book.setTitle(title);
+            book.setDescription(desc);
+            book.setLanguage(lang);
+            book.setNumberOfPages(numOfPages);
+            return bookApiRepository.save(book); 
+        } else {
+            return null;
+        }
+	}
+    
+    public void deleteBook(Long id) {
+        bookApiRepository.deleteById(id);
+    }
 	
 }
