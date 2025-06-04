e09911bd0d71b4541ac4a8583c986bfeac582393
SaherKawas <saher.kawas@gmail.com>
2025-06-04 13:02:20 +0300

    BookAPI

--- Java/Java Spring/BookAPI/src/main/java/com/axsos/bookapi/controllers/BookController.java
+++ Java/Java Spring/BookAPI/src/main/java/com/axsos/bookapi/controllers/BookController.java
@@ -1,10 +1,13 @@
 package com.axsos.bookapi.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.axsos.bookapi.models.BookApi;
 import com.axsos.bookapi.services.BookApiService;
@@ -21,6 +24,13 @@ public class BookController {
 		BookApi book =bookApiService.findBook(id);
  		model.addAttribute("book", book);
     
-        return "show.jsp";
+ 		return "show.jsp";
 	}
+	
+	@RequestMapping("/books")
+    public String index(Model model) {
+        List<BookApi> books = bookApiService.allBooks();
+        model.addAttribute("books", books);
+        return "index.jsp";
+    }
 }
